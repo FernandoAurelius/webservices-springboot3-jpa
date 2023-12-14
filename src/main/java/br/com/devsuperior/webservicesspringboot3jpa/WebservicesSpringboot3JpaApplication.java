efca4049e49470a88b56d3965fdedde90f0dc1d1
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2023-12-14 17:32:05 -0300

    Project created (Projeto criado)

---
+++ src/main/java/br/com/devsuperior/webservicesspringboot3jpa/WebservicesSpringboot3JpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.webservicesspringboot3jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesSpringboot3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesSpringboot3JpaApplication.class, args);
+	}
+
+}
