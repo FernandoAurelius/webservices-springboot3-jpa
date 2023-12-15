d839b766f6c00f22c3b694fb3e3805d26da3f6f8
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2023-12-15 18:58:00 -0300

    Service layer, component registration (Camada de serviço, registro de componente)

--- src/main/java/br/com/devsuperior/webservicesspringboot3jpa/repositories/UserRepository.java
+++ src/main/java/br/com/devsuperior/webservicesspringboot3jpa/repositories/UserRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.devsuperior.webservicesspringboot3jpa.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
 }
